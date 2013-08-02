927075d425bb4ca556f7e39fdda95e0b7ddd79d8
Daniel Díaz <dhelta.diaz@gmail.com>
2013-08-02 02:03:15 +0200

    Fixed a bug in the Simple interface. Also, now it follows the natural way of coordinates, where x coordinates increase to the top and y coordinates to the right.

--- Graphics/Web/Processing/Simple.hs
+++ Graphics/Web/Processing/Simple.hs
@@ -2,6 +2,14 @@
 -- | A 'Monoid' models figures in the plane.
 --   Then, figures are displayed or animated using
 --   a Processing script.
+--
+--   For example, this expression represents a circle
+--   of radius 10 centered at the origin:
+--
+-- > Circle (0,0) 10
+--
+--   The origin will be represented at the center of
+--   the screen.
 module Graphics.Web.Processing.Simple (
      -- * Types
      module Graphics.Web.Processing.Core.Types
@@ -19,6 +27,10 @@ module Graphics.Web.Processing.Simple (
 import Data.Monoid
 import Graphics.Web.Processing.Core.Types
 import Graphics.Web.Processing.Mid
+-- state
+import Control.Applicative ((<$>))
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Class
 
 -- | A path is just a list of points.
 type Path = [Proc_Point]
@@ -55,21 +67,78 @@ pairList :: [a] -> [(a,a)]
 pairList (x:y:zs) = (x,y) : pairList (y:zs)
 pairList _ = []
 
+-- | Adjust a point so x coordinates increase
+--   to the right and y coordinates to the top.
+adjustPoint :: Proc_Point -> Proc_Point
+adjustPoint (x,y) = (x,-y)
+
+-- | SimpleEventM monad: EventM with a state attached.
+type SimpleEventM c = StateT Settings (EventM c)
+
+data Settings = Settings {
+   currentLineColor :: Color
+ , currentFillColor :: Color
+   }
+
+defaultSettings :: Settings
+defaultSettings = Settings {
+   currentLineColor = Color   0   0   0 255 -- black
+ , currentFillColor = Color 255 255 255 255 -- white
+   }
+
+setLineColor :: Color -> SimpleEventM c ()
+setLineColor c = modify $ \s -> s { currentLineColor = c }
+
+getLineColor :: SimpleEventM c Color
+getLineColor = currentLineColor <$> get
+
+setFillColor :: Color -> SimpleEventM c ()
+setFillColor c = modify $ \s -> s { currentFillColor = c }
+
+getFillColor :: SimpleEventM c Color
+getFillColor = currentFillColor <$> get
+
+figureSEvent :: Drawing c => Figure -> SimpleEventM c ()
+-- Pictures
+figureSEvent (Line ps) = lift $ mapM_ (uncurry line) $ pairList $ fmap adjustPoint ps
+figureSEvent (Polygon ps) = lift $ polygon $ fmap adjustPoint ps
+figureSEvent (Ellipse p w h) = lift $ ellipse (adjustPoint p) w h
+figureSEvent (Circle p r) = lift $ circle (adjustPoint p) r
+figureSEvent (Arc p w h start end) = lift $ arc (adjustPoint p) w h start end
+figureSEvent (Rectangle p w h) = lift $ rect (adjustPoint p) w h
+figureSEvent (Bezier start p1 p2 end) =
+  lift $ bezier (adjustPoint start)
+                (adjustPoint p1)
+                (adjustPoint p2)
+                (adjustPoint end)
+figureSEvent (Text p t) = lift $ drawtext t (adjustPoint p) 0 0 -- font size doesn't work anyway (?)
+-- Settings
+figureSEvent (LineColor c f) = do
+  c0 <- getLineColor
+  setLineColor c
+  lift $ stroke c
+  figureSEvent f
+  setLineColor c0
+  lift $ stroke c0
+figureSEvent (FillColor c f) = do
+  c0 <- getFillColor
+  setFillColor c
+  lift $ fill c
+  figureSEvent f
+  setFillColor c0
+  lift $ fill c0
+-- Transformations
+figureSEvent (Translate (x,y) f) = lift (translate x (-y)) >> figureSEvent f >> lift (translate (-x) y)
+figureSEvent (Rotate a f) = lift (rotate a) >> figureSEvent f >> lift (rotate (-a))
+figureSEvent (Scale x y f) = lift (scale x y) >> figureSEvent f >> lift (scale (recip x) (recip y))
+-- Appending
+figureSEvent (Figures fs) = mapM_ figureSEvent fs
+
 figureEvent :: Drawing c => Figure -> EventM c ()
-figureEvent (Line ps) = mapM_ (uncurry line) $ pairList ps
-figureEvent (Polygon ps) = polygon ps
-figureEvent (Ellipse p w h) = ellipse p w h
-figureEvent (Circle p r) = circle p r
-figureEvent (Arc p w h start end) = arc p w h start end
-figureEvent (Rectangle p w h) = rect p w h
-figureEvent (Bezier start p1 p2 end) = bezier start p1 p2 end
-figureEvent (Text p t) = drawtext t p 0 0 -- font size doesn't work anyway (?)
-figureEvent (LineColor c f) = stroke c >> figureEvent f
-figureEvent (FillColor c f) = fill c >> figureEvent f
-figureEvent (Translate (x,y) f) = translate x y >> figureEvent f >> translate (-x) (-y)
-figureEvent (Rotate a f) = rotate a >> figureEvent f >> rotate (-a)
-figureEvent (Scale x y f) = scale x y >> figureEvent f >> scale (recip x) (recip y)
-figureEvent (Figures fs) = mapM_ figureEvent fs
+figureEvent f = do
+  stroke $ currentLineColor defaultSettings
+  fill   $ currentFillColor defaultSettings
+  evalStateT (figureSEvent f) defaultSettings
 
 -- | Display a figure using a Processing script.
 displayFigure ::
@@ -78,11 +147,9 @@ displayFigure ::
   -> Color -- ^ Background color.
   -> Figure -- ^ Figure to display.
   -> ProcScript
-displayFigure w h bgc f = execScriptM $ do
-  on Setup $ do
-     background bgc
-  on Draw $ do
+displayFigure w h bgc f = execScriptM $ on Draw $ do
      size (maybe screenWidth fromInt w) (maybe screenHeight fromInt h)
+     background bgc
      translate (intToFloat screenWidth/2) (intToFloat screenHeight/2)
      figureEvent f
 
